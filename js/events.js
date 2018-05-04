fba752d038e206e64a60e6339540a629dae0eba1
Mark Lawrence <lawrence.ml@gmail.com>
2018-05-04 20:20:03 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -1,7 +1,15 @@
 //define functions here
+function getIt() {
+  $('p').on('click', function(){
+    alert('Hey!')
+  })
+}
+
+function frameIt() {
+  
+}
 
 $(document).ready(function(){
-
 // call functions here
-
+getIt()
 });
